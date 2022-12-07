9b44cdd7e2035994dfced7622f31fc0d94cb9223
Santiago Weihmüller <61631736+SantiW01@users.noreply.github.com>
2022-12-07 18:19:52 -0300

    Add Operator's class

--- src/main/java/com/mycompany/calculator/Calculator.java
+++ src/main/java/com/mycompany/calculator/Calculator.java
@@ -7,12 +7,11 @@ package com.mycompany.calculator;
 public class Calculator {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
     }
 }
 
 abstract class Operator {
-    protected int[] operationTerms;
+    protected int[] operationTerms = new int[2];
     
     public abstract int GetNumbers(int index);
     
@@ -20,3 +19,65 @@ abstract class Operator {
     
  }
 
+class Plus extends Operator { 
+    @Override
+    public void setNumbers(int number, int index) {
+        this.operationTerms[index] = number;
+    }
+    
+    @Override
+    public int GetNumbers(int index) {
+        return operationTerms[index];
+    }
+}
+
+class Substracion extends Operator {
+    @Override
+    public void setNumbers(int number, int index) {
+        this.operationTerms[index] = number;
+    }
+    
+    @Override
+    public int GetNumbers(int index) {
+        return operationTerms[index];
+    }
+}
+
+class Times extends Operator {
+    @Override
+    public void setNumbers(int number, int index) {
+        this.operationTerms[index] = number;
+    }
+    
+    @Override
+    public int GetNumbers(int index) {
+        return operationTerms[index];
+    }
+}
+
+class Divide  extends Operator {
+    @Override
+    public void setNumbers(int number, int index) {
+        this.operationTerms[index] = number;
+    }
+    
+    @Override
+    public int GetNumbers(int index) {
+        return operationTerms[index];
+    }
+}
+
+
+        /*Scanner inputFirstNumber = new Scanner(System.in);
+        System.out.println("Input first number: ");
+        
+        int firstNumber = inputFirstNumber.nextInt();
+        System.out.println(firstNumber);
+        
+        Scanner inputSecondNumber = new Scanner(System.in);
+        System.out.println("Input second number: ");
+        
+        int secondNumber = inputSecondNumber.nextInt();
+        Sytem.out.println(secondNumber);*/
+
+ 
